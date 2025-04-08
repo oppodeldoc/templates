968f5c5f5a730343bef68092892c3f9cdfe051ea
Matt Glaser <matt@alephsf.com>
2025-04-07 18:22:00 -0700

    feat: Add esm node template

---
+++ template/node20-esm/function/handler.js
@@ -0,0 +1,9 @@
+export default async (event, context) => {
+  const result = {
+    'body': JSON.stringify(event.body),
+    'content-type': event.headers["content-type"]
+  }
+  return context
+    .status(200)
+    .succeed(result)
+}
